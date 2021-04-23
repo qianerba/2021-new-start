1196a7bc4c1dee899a2eb67c51882493e490965e
caol5 <caol5@ziroom.com>
2021-04-23 12:53:10 +0800

    init project

---
+++ src/main/java/com/qianerba/earth/NewStart.java
@@ -0,0 +1,16 @@
+package com.qianerba.earth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qianerba
+ */
+@SpringBootApplication
+public class NewStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewStart.class, args);
+    }
+
+}
