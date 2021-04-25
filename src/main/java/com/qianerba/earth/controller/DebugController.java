e6eda24579b596dfb3224ae7fb65bb83799e9423
caol5 <caol5@ziroom.com>
2021-04-25 15:38:02 +0800

    add debug controller

---
+++ src/main/java/com/qianerba/earth/controller/DebugController.java
@@ -0,0 +1,26 @@
+package com.qianerba.earth.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+/**
+ * @author qianerba
+ * @create 2021/4/25
+ * @describe desc your class
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "/v1/debug")
+public class DebugController {
+
+    @GetMapping(value = "/alive", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String helloGuys(HttpServletRequest request) {
+        return "Hi guys, system alive until " + LocalDateTime.now();
+    }
+}
